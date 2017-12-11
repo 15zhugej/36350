f48aa83c4e5c886b70da2bb15bce77bfa644b343
Jennifer Zhuge <jzhuge@cmu.edu>
2017-12-10 19:40:23 -0500

    yo

---
+++ hello_aliens.R
@@ -0,0 +1,6 @@
+cat(sep = "\n", "Hello aliens. This is a human from Earth.", 
+    "We are friendly and wish to peacefully communicate with your society.",
+    "How did you find this message? How can I receive messages from you?",
+    "What kind of technology do you have and is time travel possible?",
+    "Where are you located in our universe? Have you visited Earth?",
+    "Is Area 51 real?")
